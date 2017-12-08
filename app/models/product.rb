d521d1d4e3b8f3c73abb75bc6536a3a8fa0b8a27
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-08 13:16:37 -0800

    add paperclip to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
+
+  has_attached_file :image, :default_url => "/images/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
