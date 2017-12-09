f3644754d22ee72283cbc7b64896185f84489c7f
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-08 16:00:33 -0800

    fix redirect if wrong password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      redirect_to sign_up_path
     end
   end
 
