422d8ed5854a9cba0d12d8c1f3d8a179b5d1cdf7
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-08 16:02:19 -0800

    add validations for quanity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates_numericality_of :quantity, greater_than_or_equal_to: 1, message: 'Select a Quantity Larger than 0'
 end
