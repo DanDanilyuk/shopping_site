d63d812eb6ac6f553f202c504542d5338d84bc82
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-09 21:31:41 -0800

    add stripe and views

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,29 @@
+class ChargesController < ApplicationController
+  def new
+    @amount = view_context.number_with_precision(current_order.total_price, precision: 2).to_i * 100
+    @total = view_context.number_to_currency current_order.total_price
+  end
+
+  def create
+    # Amount in cents
+    @total = view_context.number_to_currency current_order.total_price
+    @amount = view_context.number_with_precision(current_order.total_price, precision: 2).to_i * 100
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+    current_order.finalize(current_user)
+    session[:order_id] = nil
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
