c95430d665a1c39b3b8e039b63878c923a375353
Dan Danilyuk <DanDanilyuk@gmail.com>
2017-12-10 19:35:55 -0800

    add admin route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :admin
   helper_method :current_order
+  helper_method :authorize
+
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
